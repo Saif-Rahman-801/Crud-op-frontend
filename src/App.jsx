37944fd57c206fec3729f96eaf28d0d332d10974
Saif-Rahman-801 <saifurs102@gmail.com>
2023-10-17 13:03:51 +0600

    post method used to post data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
+  const handleForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
 
+    // POST api
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <>
-      
+      <form onSubmit={handleForm}>
+        <input type="text" name="name" id="" />
+        <br></br>
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
